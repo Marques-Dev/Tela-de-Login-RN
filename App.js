2e3e71292408b78f70812d71fb584facbb94382c
Samuel Marques <samuelcabralmarques@gmail.com>
2020-07-16 00:38:33 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView>
+      <View>
+        <Image
+          source={require('./src/assets/logo.png')}
+        />
+      </View>
+      <View>
+        <TextInput
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={() => { }}
+        />
+
+        <TextInput
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={() => { }}
+        />
+
+        <TouchableOpacity>
+          <Text>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text>Criar conta</Text>
+        </TouchableOpacity>
+        
+      </View>
+    </KeyboardAvoidingView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
